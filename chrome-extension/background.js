7589455d546970d790d1286476c77edb2c31c2b7
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2021-01-25 00:01:26 -0500

    Updating files but not working

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() { 
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.serebii.net'},
+        pageUrl: {hostEquals: 'www.bulbapedia.bulbagarden.net'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
