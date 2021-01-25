f38a0d82313ab484c79ad636ffdb68991681bf1c
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2021-01-24 22:15:56 -0500

    Fix regex on pokemon names - highlight pokemon name instead of picture

--- chrome-extension/contentScript.js
+++ chrome-extension/contentScript.js
@@ -3,31 +3,39 @@ window.addEventListener("load", highlightDom, false);
 async function highlightDom() {
   await getPokedexData();
   const allPokemonInArea = Array.from(
-    document.querySelectorAll("#content tbody tr:nth-child(2) td a")
+    document.querySelectorAll("#content tbody tr .name")
   );
 
-  chrome.storage.local.get("pokedexData", function ({ pokedexData: allPokemon }) {
-    
-    const uncaught = getUncaughtPokemon(JSON.parse(allPokemon))
-    console.log("uncaught pokemans", uncaught);
-    console.log('serebii pokemon', allPokemonInArea)
+  chrome.storage.local.get(
+    "pokedexData",
+    function ({ pokedexData: allPokemon }) {
+      const uncaught = getUncaughtPokemon(JSON.parse(allPokemon));
+      console.log("uncaught pokemans", uncaught);
+      console.log("all pokemon in area", allPokemonInArea)
 
+      for (let i = 0; i < allPokemonInArea.length; i++) {
+        const pokemonNameLink = allPokemonInArea[i];
 
-    for (let i = 0; i < allPokemonInArea.length; i++) {
-      const pokemonNameLink = allPokemonInArea[i];
+        // is a pokemon row
 
-      // is a pokemon row
+        //   let pokemonName = pokemonNameLink.href.match(/(?<=pokedex-swsh\/).*/);
+        let pokemonName = pokemonNameLink.textContent;
+        if (!pokemonName) continue;
+        else
+          pokemonName = pokemonName
+            .toLowerCase()
+            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 
-      let pokemonName = pokemonNameLink.href.match(/(?<=pokedex-swsh\/).*/);
-      if (!pokemonName) continue;
-      else pokemonName = pokemonName[0].toLowerCase();
+        console.log("serebii mon", pokemonName);
 
-      // drop enter symbol
+        // drop enter symbol
 
-      // if pokemon is caught, mark out the row
-      if (uncaught.indexOf(pokemonName) > -1) pokemonNameLink.parentElement.style.backgroundColor = "white";
+        // if pokemon is caught, mark out the row
+        if (uncaught.indexOf(pokemonName) > -1)
+          pokemonNameLink.style.backgroundColor = "red";
+      }
     }
-  });
+  );
 }
 
 function getPokedexData() {
@@ -44,25 +52,8 @@ function getUncaughtPokemon(pokedexData) {
 
   for (let i = 0; i < pokedexData.length; i++) {
     const pokemon = pokedexData[i];
-    console.log('one single pokemon', pokemon)
     if (!pokemon["captured"])
       uncaughtPokemonNames.push(pokemon["pokemon"]["name"].toLowerCase());
   }
   return uncaughtPokemonNames;
 }
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(sendResponse);
-  console.log("ajsllsafjlksafljk;fssfajlkfslk");
-  chrome.storage.local.get("pokedexData", function (data) {
-    const rawPokedexData = data.pokedexData;
-    console.log(rawPokedexData);
-    const uncaught = getUncaughtPokemon(JSON.parse(rawPokedexData));
-    console.log(uncaught);
-    console.log("greeting", request.greeting);
-    highlightDom(uncaught, function () {
-      if (request.greeting === "highlightPokemon")
-        sendResponse({ status: "completed" });
-    });
-  });
-});
