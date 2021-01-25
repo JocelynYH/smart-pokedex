2c81fa061fe2da729ae7e0046edec64360ae31e4
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2021-01-25 00:33:46 -0500

    Finish bulbapedia version

--- chrome-extension/contentScript.js
+++ chrome-extension/contentScript.js
@@ -1,45 +1,58 @@
 window.addEventListener("load", highlightDom, false);
 
-console.log('now running')
+console.log("now running");
 async function highlightDom() {
-    console.log('running')
+  console.log("running");
 
   await getPokedexData();
-  console.log('got data')
-    const visibleEncounters = Array.from(document.querySelector("h3 #Visible_encounters").parentElement.nextElementSibling.children[0].children);
-    const randomEncounters = Array.from(document.querySelector("h3 #Random_encounters").parentElement.nextElementSibling.children[0].children);
-    const wandering = Array.from(document.querySelector("h3 #Wanderers").parentElement.nextElementSibling.nextElementSibling.children[0].children)
-    const allPokemonInArea = visibleEncounters.concat(randomEncounters.concat(wandering));
+  console.log("got data");
+  const visibleEncounters = Array.from(
+    document.querySelector("h3 #Visible_encounters").parentElement
+      .nextElementSibling.children[0].children
+  );
+  const randomEncounters = Array.from(
+    document.querySelector("h3 #Random_encounters").parentElement
+      .nextElementSibling.children[0].children
+  );
+  const wandering = Array.from(
+    document.querySelector("h3 #Wanderers").parentElement.nextElementSibling
+      .nextElementSibling.children[0].children
+  );
+  const allPokemonInArea = visibleEncounters.concat(
+    randomEncounters.concat(wandering)
+  );
 
-    console.log('all pokemons from bulbapedia', allPokemonInArea)
+  console.log("all pokemons from bulbapedia", allPokemonInArea);
 
   chrome.storage.local.get(
     "pokedexData",
     function ({ pokedexData: allPokemon }) {
-      const uncaught = getUncaughtPokemon(JSON.parse(allPokemon));
-      console.log("uncaught pokemans", uncaught);
-      console.log("all pokemon in area", allPokemonInArea)
+      const caught = getCaughtPokemon(JSON.parse(allPokemon));
+      console.log("caught pokemans", caught);
+      console.log("all pokemon in area", allPokemonInArea);
 
       for (let i = 0; i < allPokemonInArea.length; i++) {
         const row = allPokemonInArea[i];
 
-        // is a pokemon row
-        if (row.childElementCount > 6) {
-            let pokemonName = row.children[0].children[0].children[1].textContent;
-            pokemonName = pokemonName
+        // contains a pokemon. (not a header row)
+        if (row.childElementCount > 5) {
+          const spanWithPokemonName = row.querySelector(
+            "div div:nth-child(1) span"
+          );
+          if (!spanWithPokemonName) continue;
+          const pokemonName = spanWithPokemonName.textContent
             .toLowerCase()
             .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-            console.log("bulbapedia mon", pokemonName);
-            if (uncaught.indexOf(pokemonName) > -1)
-            for (let cell of Array.from(row[i].children)) {
-                cell.style.backgroundColor = 'grey' }
+
+          console.log("bulbapedia mon", pokemonName);
+
+          // if pokemon is caught, mark out the row
+          if (caught[pokemonName]) {
+            for (let cell of row.children) {
+              cell.style.backgroundColor = "grey";
+            }
+          }
         }
-
-
-        // drop enter symbol
-
-        // if pokemon is caught, mark out the row
-        
       }
     }
   );
@@ -63,4 +76,15 @@ function getUncaughtPokemon(pokedexData) {
       uncaughtPokemonNames.push(pokemon["pokemon"]["name"].toLowerCase());
   }
   return uncaughtPokemonNames;
+}
+
+function getCaughtPokemon(pokedexData) {
+    let caughtPokemonDict = {};
+
+    for (let i = 0; i < pokedexData.length; i++) {
+        const pokemon = pokedexData[i];
+        const name = pokemon["pokemon"]["name"].toLowerCase();
+        if (pokemon["captured"]) caughtPokemonDict[name] = true;
+      }
+      return caughtPokemonDict;
 }
